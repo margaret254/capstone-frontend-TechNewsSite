eff9d84e1582f9aa0651c10d3ea0cc6baf8c63f3
margaret254 <maggiemwas91@gmail.com>
2020-01-22 13:20:54 +0300

    Add a constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'capstone-app';
+export class AppComponent implements OnInit {
+  title = 'TechNews Site';
+  register;
+
+  constructor(private userService: UserService ){}
+
+  ngOnInit(){
+    this.register = {
+      username: '',
+      pasword: '',
+      email: ''
+    };
+  }
+  registerUser(){
+    this.userService.registerNewUser(this.register).subscribe(
+      response => {
+        alert("User " + this.register.username + " has been created.")
+      },
+      error => console.log('error', error)
+    );
+  }
 }
