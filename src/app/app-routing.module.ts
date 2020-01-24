2f2fb5042e5d323dc9056431c9d43b043c1ce7ce
margaret254 <maggiemwas91@gmail.com>
2020-01-24 04:23:36 +0300

    Add routing for home and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { PostsComponent} from './components/posts/posts.component';
 
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent},
+  { path: 'posts', component: PostsComponent},
+  { path: '', redirectTo:"/posts", pathMatch:"full"},
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
