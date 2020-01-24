32c47e63e3cc3c1541a168bff93daa88f23887b1
margaret254 <maggiemwas91@gmail.com>
2020-01-24 04:24:38 +0300

    Add the logic functionality of the routing link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,18 @@
 import { Component, OnInit} from '@angular/core';
-import { UserService } from './user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UserService]
 })
 export class AppComponent implements OnInit {
-  title = 'TechNews Site';
-  register;
+  
 
-  constructor(private userService: UserService ){}
+  constructor(){}
 
   ngOnInit(){
-    this.register = {
-      username: '',
-      pasword: '',
-      email: ''
-    };
+
   }
-  registerUser(){
-    this.userService.registerNewUser(this.register).subscribe(
-      response => {
-        alert("User " + this.register.username + " has been created.")
-      },
-      error => console.log('error', error)
-    );
-  }
+
 }
